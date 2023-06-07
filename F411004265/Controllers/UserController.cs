4cd4cda6e9fa4ef4c23da9eeb359a536d0919407
asd89567 <as89567123@gmail.com>
2023-06-07 20:11:27 +0800

    first

---
+++ F411004265/Controllers/UserController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace F411004265.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Indexs()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Music(String name, int count)
+        {
+
+            ViewBag.Name = name;
+            if (count == 1)
+            {
+                ViewBag.Count = "do";
+            }
+            else if (count == 2)
+            {
+                ViewBag.Count = "re";
+            }
+            else if (count == 3)
+            {
+                ViewBag.Count = "mi";
+            }
+            else if (count == 4)
+            {
+                ViewBag.Count = "fa";
+            }
+            else if (count == 5)
+            {
+                ViewBag.Count = "so";
+            }
+            else if (count == 6)
+            {
+                ViewBag.Count = "la";
+            }
+            else if (count == 7)
+            {
+                ViewBag.Count = "xi";
+            }
+
+            else
+            {
+                ViewBag.Count = "錯誤";
+            }
+            ViewBag.Count = count;
+            return View();
+        }
+    }
+}
---
+++ F411004265/Models/Partials/News.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace F411004265.Models
+{
+    [MetadataType(typeof(NewsMetadata))]
+    public partial class News
+    {
+    }
+  
+    public class NewsMetadata
+    {
+        public int Id
+        {
+            get; set;
+        }
+        [Required(ErrorMessage = "欄位標題必須要為最大長度為50的字串")]
+        [Display(Name = "標題")]
+        [StringLength(50)]
+        
+
+        public string Title
+        {
+            get; set;
+        }
+        [Required(ErrorMessage = "不能為空")]
+        [Display(Name = "內容")]
+        public string Content
+        {
+            get; set;
+        }
+        [Required(ErrorMessage = "Required")]
+        [Display(Name = "publsh")]
+        public Nullable<System.DateTime> PublishDate
+        {
+            get; set;
+        }
+    }
+
+}
